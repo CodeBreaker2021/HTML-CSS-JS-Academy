ad2cdec0b50b2a96366b8e06a191665c8a2f489c
CodeBreaker2021 <96780437+CodeBreaker2021@users.noreply.github.com>
2022-06-14 21:50:03 +0800

    HTML,CSS,JS Academy

---
+++ Coursesscript.js
@@ -0,0 +1,1 @@
+alert ('You are now in the Courses section.');
---
+++ Homescript.js
@@ -0,0 +1,9 @@
+alert ('Welcome to HML,CSS,JS Academy!')
+
+let yes = prompt('Do you want to know more about HTML,CSS,JS Academy? If Yes, type yes. Leave blank and click ok if no')
+
+if (yes) {
+    alert('Hi There! HTML,CSS,JS Academy allows you to learn HTML,CSS,JS and gives you some ideas of what you projects using just HTML,CSS and JS. Have Fun Learning and coding other projects on your own or with other tutorials that teaches how to code those projects!')
+  } else {
+    window.location.href = "index1.html";  }    
+
